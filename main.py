d9a0048dd04bde0b546a7967a638fd9956cc86d3
Antti Vainikka <antti.jk.vainikka@gmail.com>
2019-07-01 10:24:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os.path
+import random
+import re
+import requests
+
+
+def init_lunch_urls_list():
+    open('lunch_urls.txt', 'a').close()
+
+
+def get_random_url():
+    f = open('lunch_urls.txt', 'r')
+    urls = []
+    for url in f:
+        urls.append(url)
+    if len(urls) == 0:
+        raise Exception('lunch_urls.txt file is empty. Add www.lounaat.info urls to restaurants to that file.')
+    index = random.randint(0, len(urls) - 1)
+    return urls[index].strip()
+
+
+def get_menu(url):
+    page = requests.get(url)
+    if not page.ok:
+        raise Exception('Request failed')
+    contents = page.content
+    soup = BeautifulSoup(contents, 'html.parser')
+
+    # Parse restaurant's name and add it to output
+    name = soup.find('h2', {'itemprop': 'name'}).text.strip()
+    output = name
+    underline = '-' * len(name)
+    output += '\n' + underline
+
+    menu = soup.select('div.item-body')
+    menu_today = menu[datetime.today().weekday()]
+
+    # Check for missing menu
+    missing = menu_today.find('a', {'class': 'missing'})
+    if missing is not None:
+        output += '\n' + missing.text
+        return output
+
+    # Parse today's menu
+    dish_content = menu_today.find_all('p')
+    for content in dish_content:
+        dishes = content.text
+        # Clean output
+        dishes = dishes.replace(' l ', '')
+        dishes = dishes.replace(' G ', '')
+        dishes = dishes.replace(' g ', '')
+        dishes = re.sub(r'\s\s+', '\n', dishes)
+        dishes = dishes.strip()
+        output += '\n' + dishes
+    return output
+
+
+def get_random_menu():
+    try:
+        url = get_random_url()
+        print url
+        menu = get_menu(url)
+        print '\n' + menu
+    except Exception as e:
+        print e
+
+
+if os.path.exists('lunch_urls.txt'):
+    get_random_menu()
+else:
+    init_lunch_urls_list()
+    print 'lunch_urls.txt file created. Add www.lounaat.info urls to restaurants to that file.'
